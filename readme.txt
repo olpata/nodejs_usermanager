1a836d54ef4e91338ee258d0989ffabc01fc610a
ashekotilov <shekotil@ya.ru>
2018-02-13 11:39:04 +0300

    added -complite use sequelize with sqlite -singin and singup strategy with passport -hbs templates for views

--- readme.txt
+++ readme.txt
@@ -1,6 +1,17 @@
+**INFO used materials:
 https://stackoverflow.com/questions/23481817/node-js-passport-autentification-with-sqlite
 https://scotch.io/tutorials/easy-node-authentication-setup-and-local
+https://code.tutsplus.com/ru/tutorials/using-passport-with-sequelize-and-mysql--cms-27537
 
+**TODO - check and update - some packages not using - efj,promise,sqlite?
+**TODO - check migration and use install
+**TODO - check efl views and add bootstrap to view hbs
+**INFO - on git clone need create file ".env" (create file .env.example and in cmd "copy .env.example .env").
+    Add line in .env "NODE_ENV = 'development'"
+**TODO - move salt to table and generate for each user.
+
+**INFO to init nodejs enviroment:
+**INFO to import in eclipse:
 npm install express-generator -g
 express -h
 express --view=pug myapp
---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var port     = process.env.PORT ||  3000;
+var passport   = require('passport')
+var session    = require('express-session')
+var bodyParser = require('body-parser')
+var env = require('dotenv').load();
+var exphbs = require('express-handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+//For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//Models
+var models = require("./app/models");
+
+var authRoute = require('./app/routes/auth.js')(app,passport);
+//load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
+
+
+ //Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
+});
+
+app.get('/', function(req, res) {
+ 
+    res.send('Welcome to Passport with Sequelize');
+ 
+});
+ 
+ 
+app.listen(port, function(err) {
+ 
+    if (!err)
+        console.log(`Site is live (port ${port})`);
+    else console.log(err)
+ 
+});
